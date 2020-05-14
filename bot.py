4342378e4a8073e2f08afd19af2717a71bc73da9
Famiu Haque <famiuhaque@gmail.com>
2020-05-14 20:36:49 +0600

    Added basic functionality

--- bot.py
+++ bot.py
@@ -2,14 +2,12 @@ import os
 import discord
 import logging
 
-# Log all Discord logs to file discord.log
+# Basic log configuration
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
 
 client = discord.Client()
+
+guildprefix = {}
 
 @client.event
 async def on_ready():
@@ -17,7 +15,18 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('$ping'):
+    if message.content.startswith(guildprefix.get(message.guild.id, "~")):
+        respond_to_command(message)
+
+def respond_to_command(message):
+    message_words = message.split()
+    command = message_words[0][1:]
+    arguments = " ".join(message_words[1:])
+
+    if(command == "ping"):
         await message.channel.send("Pong!")
+
+    elif(command == "eval"):
+        await message.channel.send(eval(arguments))
 
 client.run(os.environ.get('BOT_TOKEN'))
